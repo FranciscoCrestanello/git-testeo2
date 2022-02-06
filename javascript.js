89e8e7cde2f9ba77a011cd4c7ed73dea98aa7086
Francisco Crestanello <crestanellofrancisco@gmail.com>
2022-02-06 10:54:35 -0300

    Iniciando proyecto

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+/*
+let nombre = "Juan";
+document.getElementById("persona").innerHTML = "Hola " + nombre;
+*/
+
+/* OPERADOR TERNARIO (MAS SENCILLO QUE IF/ELSE)
+var edad = 22;
+var puedeVotar = (edad >= 18) ? "Puede votar" : "No puede votar"
+console.log(puedeVotar);
+*/
+
+// OPERADORES DE TIPOS
